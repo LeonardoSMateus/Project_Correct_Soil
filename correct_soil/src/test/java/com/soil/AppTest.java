81174c9f2f0e5a81f57397481f2d97b23326e29a
carlosdigital <carlos.goncalves@digitalsys.com.br>
2021-03-17 17:52:02 -0300

    tests das funcoes implementadas

--- correct_soil/src/test/java/com/soil/AppTest.java
+++ correct_soil/src/test/java/com/soil/AppTest.java
@@ -1,38 +1,40 @@
 package com.soil;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+
+    public void testCtc_Cmol(){
+        Ctc_Cmol c = new Ctc_Cmol();
+        double resultado = c.CTC_cmol(1, 1, 1, 5.35);
+        assertEquals(8.35,resultado,0);
+    }
+    @Test
+
+    public void S_Cmol(){
+        S_Cmol s = new S_Cmol();
+        double resultado = s.S_cmol(1, 1, 1);
+        assertEquals(3,resultado,0);
+    }
+    
+    public void V_Atual(){
+        V_Atual v = new V_Atual();
+        double resultado = v.V_At(0,0,0,0);
+        assertEquals(0,resultado,0);
     }
 }
