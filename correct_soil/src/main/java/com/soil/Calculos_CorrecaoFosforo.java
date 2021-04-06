65d750d5780aa1ff7e746ed5d6b944bcce6d8b37
carlosdigital <carlos.goncalves@digitalsys.com.br>
2021-04-06 14:00:34 -0300

    reducao de numero de estruturas utilizando o array

--- correct_soil/src/main/java/com/soil/Calculos_CorrecaoFosforo.java
+++ correct_soil/src/main/java/com/soil/Calculos_CorrecaoFosforo.java
@@ -1,86 +1,33 @@
 package com.soil;
 
 public class Calculos_CorrecaoFosforo{
-    public double quantidadeAplicar(double teor_fosforoAtingir,double fonte_fosforo, double teor_fosforo,double eficiencia_fosforo)
+    public double quantidadeAplicar(double teor_fosforoAtingir,Integer fonte_fosforo, double teor_fosforo,double eficiencia_fosforo)
     {
-        double teor_p2O5 = 1;
+    
         double quantidade_aplicar;
+        double teor_p2O5;
+        //double[] array_resultado;
+        double[] array_resultado = {18,41,48,45,18,33,29,32,24,18.5,52,12};
+        teor_p2O5 = array_resultado[fonte_fosforo-1];
 
         if((teor_fosforoAtingir - teor_fosforo)<0.01){
             quantidade_aplicar = 0;
             return quantidade_aplicar;
         }
         else{
-            if(fonte_fosforo == 1 ||fonte_fosforo == 5 ||fonte_fosforo == 12){
-                teor_p2O5 = 18;
-            }else if(fonte_fosforo == 2){
-                teor_p2O5 = 41;
-            }
-            else if(fonte_fosforo == 3){
-                teor_p2O5 = 48;
-            }
-            else if(fonte_fosforo == 4){
-                teor_p2O5 = 45;
-            }
-            else if(fonte_fosforo == 6){
-                teor_p2O5 = 33;
-            }
-            else if(fonte_fosforo == 7){
-                teor_p2O5 = 29;
-            }
-            else if(fonte_fosforo == 8){
-                teor_p2O5 = 32;
-            }
-            else if(fonte_fosforo == 9){
-                teor_p2O5 = 24;
-            }
-            else if(fonte_fosforo == 10){
-                teor_p2O5 = 18.5;
-            }
-            else if(fonte_fosforo == 11){
-                teor_p2O5 = 52;
-            }
-    
             quantidade_aplicar = (((teor_fosforoAtingir - teor_fosforo)*2*2.29)*100/eficiencia_fosforo/100)*100/teor_p2O5;
             return quantidade_aplicar;
         }
     }
-    public double superfosfato_Simples(double teor_fosforoAtingir,double fonte_fosforo, double quantidade_aplicar, double teor_fosforo,double eficiencia_fosforo) 
+    public double superfosfato_Simples(double teor_fosforoAtingir,Integer fonte_fosforo, double quantidade_aplicar, double teor_fosforo,double eficiencia_fosforo) 
     {
-        double teor_p2O5 = 1;
-        
+        double teor_p2O5;
+        //double[] array_resultado;
+        double[] array_resultado = {18,41,48,45,18,33,29,32,24,18.5,52,12};
+        teor_p2O5 = array_resultado[fonte_fosforo-1];
         if(fonte_fosforo == 5){
             return (quantidade_aplicar*0.15);
         }else{
-            if(fonte_fosforo == 1 ||fonte_fosforo == 12){
-                teor_p2O5 = 18;
-            }else if(fonte_fosforo == 2){
-                teor_p2O5 = 41;
-            }
-            else if(fonte_fosforo == 3){
-                teor_p2O5 = 48;
-            }
-            else if(fonte_fosforo == 4){
-                teor_p2O5 = 45;
-            }
-            else if(fonte_fosforo == 6){
-                teor_p2O5 = 33;
-            }
-            else if(fonte_fosforo == 7){
-                teor_p2O5 = 29;
-            }
-            else if(fonte_fosforo == 8){
-                teor_p2O5 = 32;
-            }
-            else if(fonte_fosforo == 9){
-                teor_p2O5 = 24;
-            }
-            else if(fonte_fosforo == 10){
-                teor_p2O5 = 18.5;
-            }
-            else if(fonte_fosforo == 11){
-                teor_p2O5 = 52;
-            }
             double mmb24 = (((teor_fosforoAtingir - teor_fosforo)*2*2.29*100)/eficiencia_fosforo/100)*100/teor_p2O5*2.42;
             if(fonte_fosforo == 1){
                 return (mmb24*0.1/(2.42));
@@ -101,45 +48,11 @@ public class Calculos_CorrecaoFosforo{
         }
         return 0.0;
     }
-    public double enxofre(double quantidade_aplicar, double fonte_fosforo){
-        if(fonte_fosforo ==1)
-        {
-            return (quantidade_aplicar*0.28);
-        }else if(fonte_fosforo ==2){
-            return (quantidade_aplicar*0.2);
-        }else if(fonte_fosforo ==3){
-            return (quantidade_aplicar*0.09);
-        }
-        else if(fonte_fosforo ==4){
-            return (quantidade_aplicar*0.16);
+    public double enxofre(double quantidade_aplicar, Integer fonte_fosforo){
+        double[] array_resultado = {0,quantidade_aplicar*0.28,quantidade_aplicar*0.2,quantidade_aplicar*0.09,
+            quantidade_aplicar*0.16,quantidade_aplicar*0.28,quantidade_aplicar*0.52,quantidade_aplicar*0.53,quantidade_aplicar*0.45,quantidade_aplicar*0.28,quantidade_aplicar*0.44,0,quantidade_aplicar*0.18};
 
-        }else if(fonte_fosforo ==5){
-            return (quantidade_aplicar*0.28);
-
-        }else if(fonte_fosforo ==6){
-            return (quantidade_aplicar*0.52);
-
-        }else if(fonte_fosforo ==7){
-            return (quantidade_aplicar*0.53);
-
-        }else if(fonte_fosforo ==8){
-            return (quantidade_aplicar*0.45);
-
-        }else if(fonte_fosforo ==9){
-            return (quantidade_aplicar*0.28);
-
-        }
-        else if(fonte_fosforo ==10){
-            return (quantidade_aplicar*0.44);
-
-        }else if(fonte_fosforo ==11){
-            return (quantidade_aplicar*0.0);
-
-        }else if(fonte_fosforo ==12){
-            return (quantidade_aplicar*0.18);
-
-        }
-        return 0.0;
+        return array_resultado[fonte_fosforo];
     }
 
 }
